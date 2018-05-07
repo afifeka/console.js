3e0e346668f97030094a27a8f614fbbf0293cf13
afifeka <37196374+afifeka@users.noreply.github.com>
2018-05-07 20:13:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -154,16 +154,18 @@ bot.on("message", async message => {
   }
 	
   if(cmd === `${prefix}clear`){
-	    if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) {
       let embed = new Discord.RichEmbed()
       .setDescription("You dont have permission for run this command")
       .setColor("#ce0e00")
       return message.channel.send(embed);
+    }
     if(!args[0]) {
       let qembed = new Discord.RichEmbed()
       .setDescription("Plase give me the numbers!")
       .setColor("#ce0e00")
       return message.channel.send(qembed);
+    }
     message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages!`).then(msg => msg.delete(2000));
   });
